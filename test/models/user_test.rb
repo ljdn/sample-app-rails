41c2e6005f08a6e8632a1b0387c27b00f02af886
Lejia Duan <lejia.duan@gmail.com>
2015-07-08 15:00:58 -0500

    Add password and index

---
+++ test/models/user_test.rb
@@ -0,0 +1,59 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def setup
+  	@user = User.new(name: "Artemis", email: "huntress@olympus.mt")
+  end
+
+  test "should be valid" do
+  	assert @user.valid?
+  end
+
+  test "name present" do
+  	@user.name = "  "
+  	assert_not @user.valid?
+  end
+
+  test "email present" do
+  	@user.email = " "
+  	assert_not @user.valid?
+  end
+
+  test "name too long" do
+  	@user.name = "a" * 51
+  	assert_not @user.valid?
+  end
+
+  test "email too long" do
+  	@user.email = "a" * 244 +  "@example.com"
+  	assert_not @user.valid?
+  end
+
+  test "accept valid emails" do
+  	valid = %w[hello@me.com huntress@mt.olympus.org lem.singer@worldbuilder.sff LIFT_shardfork@roshar.wd]
+  	valid.each do |address|
+  		@user.email = address
+  		assert @user.valid?, "#{address.inspect} should be valid"
+  	end
+  end
+
+  test "reject invalid emails" do
+  	invalid = %w[goaway@this,iswrong blah_blah_blah.org jean.valjean@lesmis. meow@grumpy_cat.com boo@x+y.com]
+  	invalid.each do |address|
+  		@user.email = address
+  		assert_not @user.valid? "#{address.inspect} should be invalid"
+  	end
+  end
+
+  test "unique email address" do
+  	duplicate = @user.dup
+  	duplicate.email = @user.email.upcase
+  	@user.save
+  	assert_not duplicate.valid?
+  end
+
+end
